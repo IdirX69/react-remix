8d44b1fbe2bacad3fa26a2911911c1d2ca5ffe0e
IdirX69 <adil.idirr@gmail.com>
2024-03-11 18:17:37 +0100

    login form test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { Form, json } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,12 +7,27 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const jsonData = Object.fromEntries(formData);
+  const response = await fetch("http://localhost:5000/auth/login", {
+    method: "POST",
+    body: JSON.stringify(jsonData),
+  });
+  const token = await response.json();
+  console.log(token);
+  return json({});
+};
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
-      <Form></Form>
+      <Form method="POST">
+        <input type="email" name="email" required />
+        <input type="password" name="password" required />
+        <button type="submit">Submit</button>
+      </Form>
     </div>
   );
 }
