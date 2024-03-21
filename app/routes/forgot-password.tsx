ecc9f43e757049aee3f08c2e2940e9601b49cd26
IdirX69 <adil.idirr@gmail.com>
2024-03-21 20:59:56 +0100

    forgot-password

---
+++ app/routes/forgot-password.tsx
@@ -0,0 +1,73 @@
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { Form, json, redirect, useLoaderData } from "@remix-run/react";
+import { getAuthenticatedUser } from "~/auth.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const user = await getAuthenticatedUser({ request });
+  if (user) {
+    redirect("/");
+  }
+  try {
+    const urlParams = new URL(request.url).searchParams;
+    const token = urlParams.get("token");
+
+    const response = await fetch(
+      `http://localhost:5000/auth/verify-reset-password-token?token=${token}`,
+      {
+        headers: { "Content-type": "application/json" },
+      }
+    );
+    const { error, message } = await response.json();
+    if (error) {
+      return json({ error, message, token });
+    }
+  } catch (error) {
+    const err = error as Error;
+    return json({ error: true, message: err.message, token: "" });
+  }
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const jsonData = Object.fromEntries(formData);
+
+  const response = await fetch(
+    "http://localhost:5000/auth/request-reset-password",
+    {
+      method: "POST",
+      body: JSON.stringify(jsonData),
+      headers: { "Content-type": "application/json" },
+    }
+  );
+
+  const { error, message } = await response.json();
+  if (error) {
+    return json({ error, message });
+  }
+};
+
+export default function ForgotPasswordForm() {
+  const { error, message, token } = useLoaderData<typeof loader>();
+  console.log(message);
+
+  if (!token) {
+    return (
+      <>
+        <Form method="POST">
+          <h1>Mot de passe oubliée</h1>
+          <input type="email" name="email" required placeholder="Votre email" />
+
+          <button type="submit">Récuperer mon mot de passe</button>
+        </Form>
+      </>
+    );
+  }
+  if (token && error === true) {
+    return (
+      <>
+        {error?.message ? <p>Error:{error}</p> : null}
+        Message:{message}
+      </>
+    );
+  }
+}
